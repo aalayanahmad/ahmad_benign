568910965f2a7734fcabbf6fef355947091838c9
umroot <umroot@designer.encs.concordia.ca>
2024-06-22 21:20:13 -0400

    cleaning up ricardo's code

--- execute.py
+++ execute.py
@@ -34,95 +34,165 @@ def run_command(command):
 while t <= 7230: #2700,3600
     t_plus2 = t + 2
     print("-------------time;", str(t), "---------->+2 seconds:", str(t + 2) + "-------------")
-    UEevents = [ue for ue in ue_list if t <= math.floor(float(ue[2])) < t_plus2]
+    ue_events = [ue for ue in ue_list if t <= math.floor(float(ue[2])) < t_plus2]
     processes = []
 
-    for ue in UEevents:
-        print(ue)
-        if int(ue[1]) == 1:
-         try:
-            cr += 1
-            print("registration")
-            tr += 1
-            filename="ue"+ue[0]+".txt"
-            file = open(filename, "w")
-            
-            imsivalue=imsi+ue[0]
-            print("VALUE OF CR(registration):"+str(tr))
-            file.write(imsivalue+ "," +str(tr)+ "\n")
-            #Close the file
-            file.close()
-       
-            p = Process(target=run_command, args=(['bash', './iss.sh', imsi + ue[0], ue[0],str(7777)],))
-            processes.append(p)
-         except:
-            print("error in iss")   
-        elif int(ue[1]) == 2:
-         try:        
-            cr += 1
-            print("RegGGGGGGGGGGGGGGGGGGGGG")
-            tr += 1
-            filename="ue"+ue[0]+".txt"
-            file = open(filename, "w")
-            #Write content to the file
-            imsivalue=imsi+ue[0]
-            print("VALUE OF CR(REG):"+str(tr))
-            file.write(imsivalue+ "," +str(tr)+ "\n")
-            #Close the file
-            file.close()
-            
-            p = Process(target=run_command, args=(['bash', './register.sh', ue[0]],))
-            processes.append(p)
-         except:
-            print("error in registration")   
-        elif int(ue[1]) == 3:
-         try:
-            cu += 1
-            p = Process(target=run_command, args=(['bash', './uplink.sh', imsi + ue[0]],))
-            processes.append(p)
-         except:
-            print("error in uplink")   
-        elif int(ue[1]) == 4:
-         try:        
-            cdw += 1
-            p = Process(target=run_command, args=(['bash', './downlink.sh', imsi + ue[0]],))
-            processes.append(p)
-         except:
-            print("error in donwlink")   
-        elif int(ue[1]) == 5:
-         try:        
-            cp += 1
-            p = Process(target=run_command, args=(['bash', './ueRelease.sh', imsi + ue[0]],))
-            processes.append(p)
-         except:
-            print("error in PDU session release")   
-        elif int(ue[1]) == 6:
-         try:        
-            cid += 1
-            # imsi + ue[0],it was deleted becaseu gnb was modified.
-            p = Process(target=run_command, args=(['bash', './gnbBRelease.sh', imsi + ue[0], ue[0]],))
-            processes.append(p)
-         except:
-            print("error in GNB release")   
-        else:
-         try:          
-            cd += 1
-            p = Process(target=run_command, args=(['bash', './Deregister.sh', imsi + ue[0]],))
-            processes.append(p)
-         except:
-            print("error in deregistration")     
-
+    for ue in ue_events:
+        match ue[1]:
+         case 1:
+            try:
+               cr += 1
+               print("registration")
+               tr += 1
+               file_name = "ue" + ue[0] + ".txt"
+               file = open(file_name, "w")
+               
+               imsi_value = imsi+ue[0]
+               file.write(imsi_value + "," +str(tr) + "\n")
+               file.close()
+               p = Process(target=run_command, args=(['bash', './register.sh', imsi + ue[0],],))
+               processes.append(p)
+               continue
+            except:
+               print("error in registration")   
+         case 2:
+            try:        
+               cua += 1
+               print("uplink_any")
+               tr += 1
+               file_name = "ue" + ue[0] + ".txt"
+               file = open(file_name, "w")
+               imsi_value = imsi + ue[0]
+               file.write(imsi_value+ "," + str(tr) + "\n")
+               file.close()
+               p = Process(target=run_command, args=(['bash', './uplink_any.sh', ue[0]],))
+               processes.append(p)
+               continue
+            except:
+               print("error in uplink_any")   
+         case 3:
+            try:        
+               cus1 += 1
+               print("uplink_service1")
+               tr += 1
+               file_name = "ue" + ue[0] + ".txt"
+               file = open(file_name, "w")
+               imsi_value = imsi + ue[0]
+               file.write(imsi_value+ "," + str(tr) + "\n")
+               file.close()
+               p = Process(target=run_command, args=(['bash', './uplink_service1.sh', ue[0]],))
+               processes.append(p)
+               continue
+            except:
+               print("error in uplink_service1")   
+         case 4:
+            try:        
+               cus2 += 1
+               print("uplink_service2")
+               tr += 1
+               file_name = "ue" + ue[0] + ".txt"
+               file = open(file_name, "w")
+               imsi_value = imsi + ue[0]
+               file.write(imsi_value+ "," + str(tr) + "\n")
+               file.close()
+               p = Process(target=run_command, args=(['bash', './uplink_service2.sh', ue[0]],))
+               processes.append(p)
+               continue
+            except:
+               print("error in uplink_service2") 
+         case 5:
+            try:        
+               cus1d += 1
+               print("uplink_service1_delayed")
+               tr += 1
+               file_name = "ue" + ue[0] + ".txt"
+               file = open(file_name, "w")
+               imsi_value = imsi + ue[0]
+               file.write(imsi_value+ "," + str(tr) + "\n")
+               file.close()
+               p = Process(target=run_command, args=(['bash', './uplink_service1_delayed.sh', ue[0]],))
+               processes.append(p)
+               continue
+            except:
+               print("error in uplink_service1_delayed") 
+         case 6:
+            try:        
+               cus2d += 1
+               print("uplink_service2_delayed")
+               tr += 1
+               file_name = "ue" + ue[0] + ".txt"
+               file = open(file_name, "w")
+               imsi_value = imsi + ue[0]
+               file.write(imsi_value+ "," + str(tr) + "\n")
+               file.close()
+               p = Process(target=run_command, args=(['bash', './uplink_service2_delayed.sh', ue[0]],))
+               processes.append(p)
+               continue
+            except:
+               print("error in uplink_service2_delayed")  
+         case 7:
+            try:        
+               cd += 1
+               print("downlink")
+               tr += 1
+               file_name = "ue" + ue[0] + ".txt"
+               file = open(file_name, "w")
+               imsi_value = imsi + ue[0]
+               file.write(imsi_value+ "," + str(tr) + "\n")
+               file.close()
+               p = Process(target=run_command, args=(['bash', './downlink.sh', ue[0]],))
+               processes.append(p)
+               continue
+            except:
+               print("error in downlink")     
+         case 8:
+            try:        
+               cd += 1
+               print("pdu_ue_release")
+               tr += 1
+               file_name = "ue" + ue[0] + ".txt"
+               file = open(file_name, "w")
+               imsi_value = imsi + ue[0]
+               file.write(imsi_value+ "," + str(tr) + "\n")
+               file.close()
+               p = Process(target=run_command, args=(['bash', './pdu_ue_release.sh', ue[0]],))
+               processes.append(p)
+               continue
+            except:
+               print("error in pdu_ue_release")    
+         case 9:
+            try:        
+               cd += 1
+               print("pdu_gnb_release")
+               tr += 1
+               file_name = "ue" + ue[0] + ".txt"
+               file = open(file_name, "w")
+               imsi_value = imsi + ue[0]
+               file.write(imsi_value+ "," + str(tr) + "\n")
+               file.close()
+               p = Process(target=run_command, args=(['bash', './pdu_gnb_release.sh', ue[0]],))
+               processes.append(p)
+               continue
+            except:
+               print("error in pdu_gnb_release")    
+         case 10:
+            try:        
+               cd += 1
+               print("deregistration")
+               tr += 1
+               file_name = "ue" + ue[0] + ".txt"
+               file = open(file_name, "w")
+               imsi_value = imsi + ue[0]
+               file.write(imsi_value+ "," + str(tr) + "\n")
+               file.close()
+               p = Process(target=run_command, args=(['bash', './deregister.sh', ue[0]],))
+               processes.append(p)
+               continue
+            except:
+               print("error in deregistration")  
     for p in processes:
        p.start()
 
-    #for p in processes:
-     #   p.join()
 
     time.sleep(2)
     t += 2
-
-print("ci:" + str(ci) + "   cr:" + str(cr) + "   cu:" + str(cu) + "   cdw:" + str(cdw) + "   cp:" + str(cp) + "   cid:" + str(cid) + "   cd:" + str(cd))
-"""
-import library as lb
-list_events=lb.non_compromised_ues_events(0)
-"""
