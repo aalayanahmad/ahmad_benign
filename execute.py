3e6f395b9abfc9658118c452236e3c67e18a14d1
umroot <umroot@designer.encs.concordia.ca>
2024-06-30 15:53:20 -0400

    hi

--- execute.py
+++ execute.py
@@ -10,7 +10,7 @@ t = 0  # Start time
 tr = 1  # Transaction ID
 
 ue_list = []
-with open("'/ueransim/benign_events", "r") as file:
+with open("/ueransim/benign_events", "r") as file:
     lines = file.readlines()
     for line in lines:
         x = line.strip().split(",", 4)
@@ -27,7 +27,7 @@ def run_command(command):
 def process_ue_event(ue, tr):
     global cr, cua, cus1, cus2, cus1d, cus2d, cd
     event_type = ue[1]
-    file_name = f"ue{ue[0]}.txt"
+    file_name = f"ue{ue[0]}.yaml"
     imsi = imsi1 if len(ue[0]) == 3 else imsi2
     imsi_value = imsi + ue[0]
 
@@ -38,70 +38,70 @@ def process_ue_event(ue, tr):
             tr += 1
             with open(file_name, "w") as file:
                 file.write(f"{imsi_value},{tr}\n")
-            run_command(['bash', '/ueransim/register.sh', file_name])
+            run_command(['bash', '/ueransim/scripts/register.sh', file_name])
         elif event_type == "2":
             cua += 1
             print("uplink_any")
             tr += 1
             with open(file_name, "w") as file:
                 file.write(f"{imsi_value},{tr}\n")
-            run_command(['bash', '/ueransim/uplink_any.sh', imsi_value])
+            run_command(['bash', '/ueransim/scripts/uplink_any.sh', imsi_value])
         elif event_type == "3":
             cus1 += 1
             print("uplink_service1")
             tr += 1
             with open(file_name, "w") as file:
                 file.write(f"{imsi_value},{tr}\n")
-            run_command(['bash', '/ueransim/uplink_service1_benign.sh', imsi_value])
+            run_command(['bash', '/ueransim/scripts/uplink_service1_benign.sh', imsi_value])
         elif event_type == "4":
             cus2 += 1
             print("uplink_service2")
             tr += 1
             with open(file_name, "w") as file:
                 file.write(f"{imsi_value},{tr}\n")
-            run_command(['bash', '/ueransim/uplink_service2_benign.sh', imsi_value])
+            run_command(['bash', '/ueransim/scripts/uplink_service2_benign.sh', imsi_value])
         elif event_type == "5":
             cus1d += 1
             print("uplink_service1_delayed")
             tr += 1
             with open(file_name, "w") as file:
                 file.write(f"{imsi_value},{tr}\n")
-            run_command(['bash', '/ueransim/uplink_service1_delayed.sh', imsi_value])
+            run_command(['bash', '/ueransim/scripts/uplink_service1_delayed.sh', imsi_value])
         elif event_type == "6":
             cus2d += 1
             print("uplink_service2_delayed")
             tr += 1
             with open(file_name, "w") as file:
                 file.write(f"{imsi_value},{tr}\n")
-            run_command(['bash', '/ueransim/uplink_service2_delayed.sh', imsi_value])
+            run_command(['bash', '/ueransim/scripts/uplink_service2_delayed.sh', imsi_value])
         elif event_type == "7":
             cd += 1
             print("downlink")
             tr += 1
             with open(file_name, "w") as file:
                 file.write(f"{imsi_value},{tr}\n")
-            run_command(['bash', '/ueransim/downlink.sh', imsi_value])
+            run_command(['bash', '/ueransim/scripts/downlink.sh', imsi_value])
         elif event_type == "8":
             cd += 1
             print("pdu_ue_release")
             tr += 1
             with open(file_name, "w") as file:
                 file.write(f"{imsi_value},{tr}\n")
-            run_command(['bash', '/ueransim/pdu_ue_release.sh', imsi_value])
+            run_command(['bash', '/ueransim/scripts/pdu_ue_release.sh', imsi_value])
         elif event_type == "9":
             cd += 1
             print("pdu_gnb_release")
             tr += 1
             with open(file_name, "w") as file:
                 file.write(f"{imsi_value},{tr}\n")
-            run_command(['bash', '/ueransim/pdu_gnb_release.sh', imsi_value, ue[0]])
+            run_command(['bash', '/ueransim/scripts/pdu_gnb_release.sh', imsi_value, ue[0]])
         elif event_type == "10":
             cd += 1
             print("deregistration")
             tr += 1
             with open(file_name, "w") as file:
                 file.write(f"{imsi_value},{tr}\n")
-            run_command(['bash', '/ueransim/deregister.sh', imsi_value])
+            run_command(['bash', '/ueransim/scripts/deregister.sh', imsi_value])
     except Exception as e:
         print(f"Error processing event {event_type} for UE {ue[0]}: {e}")
 
@@ -116,10 +116,10 @@ def main():
             ue_events = [ue for ue in ue_list if t <= math.floor(float(ue[2])) < t_plus2]
             for ue in ue_events:
                 pool.apply_async(process_ue_event, args=(ue, tr))
-            pool.close()
-            pool.join()
             time.sleep(2)
             t += 2
+        pool.close()
+        pool.join()
 
 
 if __name__ == '__main__':
