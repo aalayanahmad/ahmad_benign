25fde719fa956bd6059283b619009a6c9486f6b4
umroot <umroot@designer.encs.concordia.ca>
2024-07-03 19:55:11 -0400

    furhter fixing scripts and execute

--- execute.py
+++ execute.py
@@ -23,7 +23,7 @@ tr = 1
 ue_list = []
 gnb_ueID_map = {}  # to store gnb id
 
-with open("/home/ubuntu/UERANSIM/ahmad_benign/benign_events", "r") as file:
+with open("/home/ubuntu/UERANSIM/benign_events", "r") as file:
     lines = file.readlines()
     for line in lines:
         x = line.strip().split(",", 4)
@@ -48,36 +48,36 @@ while t <= 7230:
                 cr += 1
                 gnb_ueID_map[ue[0]] = str(cr)
                 tr += 1
-                p = Process(target=run_command, args=(['bash', '/home/ubuntu/UERANSIM/ahmad_benign/scripts/register.sh', ue[0]],))
+                p = Process(target=run_command, args=(['bash', '/home/ubuntu/UERANSIM/scripts/register.sh', ue[0]],))
             elif event == "2":
                 cua += 1
                 tr += 1
-                p = Process(target=run_command, args=(['bash', '/home/ubuntu/UERANSIM/ahmad_benign/scripts/uplink_any.sh', imsi_value],))
+                p = Process(target=run_command, args=(['bash', '/home/ubuntu/UERANSIM/scripts/uplink_any.sh', imsi_value],))
             elif event == "3":
                 cus1 += 1
-                p = Process(target=run_command, args=(['bash', '/home/ubuntu/UERANSIM/ahmad_benign/scripts/uplink_service1_benign.sh', imsi_value],))
+                p = Process(target=run_command, args=(['bash', '/home/ubuntu/UERANSIM/scripts/uplink_service1_benign.sh', imsi_value],))
             elif event == "4":
                 cus2 += 1
-                p = Process(target=run_command, args=(['bash', '/home/ubuntu/UERANSIM/ahmad_benign/scripts/uplink_service2_benign.sh', imsi_value],))
+                p = Process(target=run_command, args=(['bash', '/home/ubuntu/UERANSIM/scripts/uplink_service2_benign.sh', imsi_value],))
             elif event == "5":
                 cus1d += 1
-                p = Process(target=run_command, args=(['bash', '/home/ubuntu/UERANSIM/ahmad_benign/scripts/uplink_service1_delayed.sh', imsi_value],))
+                p = Process(target=run_command, args=(['bash', '/home/ubuntu/UERANSIM/scripts/uplink_service1_delayed.sh', imsi_value],))
             elif event == "6":
                 cus2d += 1
-                p = Process(target=run_command, args=(['bash', '/home/ubuntu/UERANSIM/ahmad_benign/scripts/uplink_service2_delayed.sh', imsi_value],))
+                p = Process(target=run_command, args=(['bash', '/home/ubuntu/UERANSIM/scripts/uplink_service2_delayed.sh', imsi_value],))
             elif event == "7":
                 cdl += 1
-                p = Process(target=run_command, args=(['bash', '/home/ubuntu/UERANSIM/ahmad_benign/scripts/downlink.sh', imsi_value],))
+                p = Process(target=run_command, args=(['bash', '/home/ubuntu/UERANSIM/scripts/downlink.sh', imsi_value],))
             elif event == "8":
                 cpu += 1
-                p = Process(target=run_command, args=(['bash', '/home/ubuntu/UERANSIM/ahmad_benign/scripts/pdu_ue_release.sh', imsi_value],))
+                p = Process(target=run_command, args=(['bash', '/home/ubuntu/UERANSIM/scripts/pdu_ue_release.sh', imsi_value],))
             elif event == "9":
                 cpg += 1
                 ue_id = gnb_ueID_map[ue[0]]
-                p = Process(target=run_command, args=(['bash', '/home/ubuntu/UERANSIM/ahmad_benign/scripts/pdu_gnb_release.sh', imsi_value, ue_id],))
+                p = Process(target=run_command, args=(['bash', '/home/ubuntu/UERANSIM/scripts/pdu_gnb_release.sh', imsi_value, ue_id],))
             else:
                 cdr += 1
-                p = Process(target=run_command, args=(['bash', '/home/ubuntu/UERANSIM/ahmad_benign/scripts/deregister.sh', imsi_value],))
+                p = Process(target=run_command, args=(['bash', '/home/ubuntu/UERANSIM/scripts/deregister.sh', imsi_value],))
             processes.append(p)
         except KeyError as ke:
             print(f"KeyError: {ke}")
