9430c774fde99e3519ef7416ea42073cbf97d8a7
umroot <umroot@designer.encs.concordia.ca>
2024-09-27 15:01:53 -0400

    fixed constants file

--- constants.py
+++ constants.py
@@ -16,31 +16,23 @@ PROCESSING_TIME_PER_EVENT = { #event: time to process the event
     }
 
 LEGAL_NEXT_EVENTS_PER_CURRENT_EVENT = { 
-    1 : [2, 3, 4, 5, 6, 7, 8, 9], #REGISTER
-    2 : [3, 4, 5, 6, 7, 8, 9], #UPLINK_ANY
-    3 : [2, 4, 5, 6, 7, 8, 9], #UPLINK_SERVICE1 
-    4:  [2, 3, 5, 6, 7, 8, 9], #UPLINK_SERVICE2
-    5 : [2, 3, 4, 6, 7, 8, 9], #UPLINK_SERVICE1_DELAYED
-    6 : [2, 3, 4, 5, 7, 8, 9], #UPLINK_SERVICE2_DELAYED
-    7 : [2, 3, 4, 5, 6, 8, 9], #DOWNLINK
-    8 : [2, 3, 4, 5, 6, 7, 9], #PDU_UE_RELEASE
-    9 : [2, 3, 4, 5, 6, 7], #PDU_GNB_RELEASE
-    10 : [1]  #DEREGISTER
+    1 : [2, 3, 4, 5], #REGISTER
+    2 : [3, 4, 5], #UPLINK
+    3 : [2, 4, 5], #DOWNLINK
+    4 : [2, 3, 5], #PDU_UE_RELEASE
+    5 : [2, 3], #PDU_GNB_RELEASE
+    6 : [1]  #DEREGISTER
 }
 
 #BENIGN PROBABILITIES
 #in a bengin scenario everything is equally probable!
 PRBABILITIES_OF_LEGAL_NEXT_EVENTS_PER_CURRENT_EVENT_BENIGN = { 
-    1 : [0.125, 0.125, 0.125, 0.125, 0.125, 0.125, 0.125, 0.125], #REGISTER
-    2 : [0.14, 0.14, 0.14, 0.14 , 0.14 , 0.14, 0.16], #UPLINK_ANY
-    3 : [0.14, 0.14, 0.14, 0.14 , 0.14 , 0.14, 0.16], #UPLINK_SERVICE1 
-    4:  [0.14, 0.14, 0.14, 0.14 , 0.14 , 0.14, 0.16], #UPLINK_SERVICE2
-    5 : [0.14, 0.14, 0.14, 0.14 , 0.14 , 0.14, 0.16], #UPLINK_SERVICE1_DELAYED
-    6 : [0.14, 0.14, 0.14, 0.14 , 0.14 , 0.14, 0.16], #UPLINK_SERVICE2_DELAYED
-    7 : [0.14, 0.14, 0.14, 0.14 , 0.14 , 0.14, 0.16], #DOWNLINK
-    8 : [0.14, 0.14, 0.14, 0.14 , 0.14 , 0.14, 0.16], #PDU_UE_RELEASE
-    9 : [0.16, 0.16, 0.16, 0.16, 0.16, 0.2], #PDU_GNB_RELEASE
-    10 : [1] #DEREGISTER
+    1 : [0.25, 0.25, 0.25, 0.25], #REGISTER
+    2 : [0.33, 0.33, 0.34], #UPLINK
+    3 : [0.33, 0.33, 0.34], #DOWNLINK
+    4 : [0.33, 0.33, 0.34], #PDU_UE_RELEASE
+    5 : [0.5, 0.5], #PDU_GNB_RELEASE
+    6 : [1] #DEREGISTER
 }
 
 LIST_OF_LAMBDAS = [1,3,5,7,5,3,1]
