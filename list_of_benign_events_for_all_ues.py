d9d8730f3c663a80c4cf78dc694cdcd04e96474b
umroot <umroot@designer.encs.concordia.ca>
2024-10-04 13:56:00 -0400

    fixed path

--- list_of_benign_events_for_all_ues.py
+++ list_of_benign_events_for_all_ues.py
@@ -44,7 +44,7 @@ def list_of_benign_events_for_all_ues():
 
     list_to_execute.sort(key = take_fourth)
 
-    with open('./list_of_benign_events_for_all_ues', 'w') as file:
+    with open('./files to execute/list_of_benign_events_for_all_ues', 'w') as file:
         for line in list_to_execute:
           file.write(','.join(str(item) for item in line))
           file.write("\n" )
